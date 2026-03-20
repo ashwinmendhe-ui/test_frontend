5535e5176b5e59fc72d082ff1273108044adc8ab
Mckinley Rice <mckinleyrice@Mckinleys-MacBook-Air.local>
2026-03-20 14:20:01 +0530

    added sidebar menu

---
+++ src/constants/menu.ts
@@ -0,0 +1,17 @@
+export const menuItems = [
+  {
+    name: "Dashboard",
+    path: "/dashboard",
+    icon: "📊",
+  },
+  {
+    name: "History",
+    path: "/history",
+    icon: "📜",
+  },
+  {
+    name: "Stream",
+    path: "/stream",
+    icon: "📺",
+  },
+]; 
